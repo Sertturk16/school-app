12e796f23714eec988f52cb4d62a5eeebed1f251
Sertturk16 <88446918+Sertturk16@users.noreply.github.com>
2021-09-21 13:59:50 +0300

    Update mail.php

--- mail.php
+++ mail.php
@@ -8,6 +8,9 @@ use PHPMailer\PHPMailer\Exception;
 //Load Composer's autoloader
 require ('PHPMailer/PHPMailer.php');
 require ('PHPMailer/SMTP.php');
+require ('PHPMailer/Exception.php');
+
+$isim = $_POST['name'];
 
 //Create an instance; passing `true` enables exceptions
 $mail = new PHPMailer(true);
@@ -30,11 +33,12 @@ try {
     //Content
     $mail->isHTML(true);                                  //Set email format to HTML
     $mail->Subject = 'Here is the subject';
-    $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
-    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+    $mail->Body    =  'Isim: ' .$isim;
 
     $mail->send();
     echo 'Message has been sent';
 } catch (Exception $e) {
     echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
 }
+
+?>
